30db698520aa52bcf8fefe80e69a7ee9bd6d059a
Kaibing <2656226581@qq.com>
2026-06-27 21:49:01 +0800

    为lead_agent添加了内置工具

---
+++ lead_agent/tools/builtins/__init__.py
@@ -0,0 +1,4 @@
+from lead_agent.tools.builtins.present_file_tool import present_file_tool
+from lead_agent.tools.builtins.view_image_tool import view_image_tool
+
+__all__ = ["present_file_tool", "view_image_tool"]
